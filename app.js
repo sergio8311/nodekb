e597a9367492c24ee9ee838908d1920a18b0e0eb
sergii <sergey8311@gmail.com>
2017-10-19 00:12:46 +0300

    add a express const and hello world example

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res){
+  res.send('Hello');
+});
+
+app.listen(3000, function(){
+  console.log('Server started on port 3000...');
+});
