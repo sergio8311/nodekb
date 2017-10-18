1bc1dd6f4dca4c22712b520f3fb9296d2a5e2e1e
sergii <sergey8311@gmail.com>
2017-10-19 00:23:32 +0300

    change function representation

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function (req, res){
-  res.send('Hello');
+app.get('/', (req, res) => {
+  res.send('Hello world!!');
 });
 
-app.listen(3000, function(){
+app.listen(3000, () => {
   console.log('Server started on port 3000...');
 });
